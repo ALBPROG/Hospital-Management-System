18cf8dcb53153d7bb0b2b7b047f9b02c2cded79e
ALBPROG <oltias1987@gmail.com>
2018-09-04 23:32:10 -0400

    new heroku cleardb configuration

---
+++ database.php
@@ -0,0 +1,32 @@
+<?php
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+
+//$conn = new mysqli($server, $username, $password, $db);
+
+$active_group = 'default';
+$active_record = TRUE;
+
+$db['default']['hostname'] = $cleardb_server;
+$db['default']['username'] = $cleardb_username;
+$db['default']['password'] = $cleardb_password;
+$db['default']['database'] = $cleardb_db;
+$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbprefix'] = '';
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_cl';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
+
+
+
+?>
