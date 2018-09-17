890342ea5190335fa5bbdac05be1f79999e0fbb3
ALBPROG <oltias1987@gmail.com>
2018-09-17 01:24:22 -0400

    add

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,9 @@
 <?php
-$con = mysqli_connect('localhost','id6216490_olti1987','Xhuxhi1987');
+$con = mysqli_connect('localhost','root','');
 if (empty($con)) {
  	echo mysql_error();
  } 
- $data = mysqli_select_db($con,"id6216490_hospital");
+ $data = mysqli_select_db($con,"hospital");
  if (empty($data)) {
  	echo mysql_error();
  }
