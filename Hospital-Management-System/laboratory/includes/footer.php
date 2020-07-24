9a3858a7ba6a4f6e6105c7be8fdcecde1931ebc2
Olti Asllanaj <oltias1987@gmail.com>
2020-07-24 00:17:39 -0400

    Update Hospital-management-system

--- Hospital-Management-System/laboratory/includes/footer.php
+++
@@ -1,5 +0,0 @@
-<div class="footer">
-	<center>
-		<h5 style="color:white;"> &copy; <?php echo date("Y"); ?> By Olti Asllanaj</h5>
-	</center>
-</div>
--- Hospital-Management-System/logout.php
+++
@@ -1,4 +0,0 @@
-<?php 
-require 'includes/users.php';
-logout();
-?>
